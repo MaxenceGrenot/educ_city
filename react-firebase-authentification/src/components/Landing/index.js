c4c0727dc824cde7e69d85f1c34b170d3a1b9df5
DuarteDosSantosNuno <67701344+DuarteDosSantosNuno@users.noreply.github.com>
2020-11-04 12:13:44 +0100

    [Auth] Conception du système d'authentification

--- react-firebase-authentification/src/components/Landing/index.js
+++ react-firebase-authentification/src/components/Landing/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Navigation = ({ authUser }) => (
-    <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+import { withAuthorization } from '../Session';
+
+const LandingPage = () => (
+    <div>
+        <h1>Landing Page</h1>
+        <p>I don't know what is his utility.</p>
+        <img src="https://image.shutterstock.com/image-photo/flat-lay-green-leaf-isolated-260nw-1015711927.jpg"></img>
+    </div>
 );
 
-export default Landing;
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(LandingPage);
--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+
     </div>
   );
 }
