f573b5996e9f3cafd4e133fe0ffe95d318667da0
Jérémie <jeremielegrand021@gmail.com>
2020-11-04 11:55:55 +0100

    [NAVBAR] Import react-bootstrap  and create navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Collapse,
   Navbar,
@@ -11,35 +11,42 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  NavbarText
-} from 'reactstrap';
-import { NavLink as RRNavLink } from 'react-router-dom';
-
-import * as ROUTES from '../constants/routes';
+  NavbarText,
+  Button,
+  Form,
+  FormControl,
+  NavDropdown,
+} from "react-bootstrap";
+import { NavLink as RRNavLink } from "react-router-dom";
+import * as ROUTES from "../constants/routes";
+import * as CONFIG from "../constants/config";
 
 const TopNav = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const toggle = () => setIsOpen(!isOpen);
-  
-    return (
-      <div>
-        <Navbar color="primary" dark expand="md">
-          <NavbarBrand href="/">Educ'City</NavbarBrand>
-          <NavbarToggler onClick={toggle} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="mr-auto" navbar>
-              <NavItem>
-                <NavLink tag={RRNavLink} exact to={ROUTES.HOME}>Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/MaxenceGrenot/educ_city" target="_blank">GitHub</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>   
-        </Navbar>
-      </div>
-    );
-  }
-  
-  export default TopNav;
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+        <Navbar.Brand href={ROUTES.HOME}>{CONFIG.WEBNAME}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">Classes</Nav.Link>
+            <Nav.Link href="#pricing">Games</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href={ROUTES.SIGN_IN}>
+              <Button>Signin</Button>
+            </Nav.Link>
+            <Nav.Link eventKey={2} href={ROUTES.SIGN_UP}>
+              <Button>Signup</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+};
+
+export default TopNav;
---
+++ src/constants/config.js
@@ -0,0 +1,1 @@
+export const WEBNAME = 'Educ\'City';
