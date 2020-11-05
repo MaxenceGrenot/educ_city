c23dbbf2a1a6002571835ad29efa521a37734482
Jérémie <jeremielegrand021@gmail.com>
2020-11-05 10:46:43 +0100

    [ROUTES] use react router

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,45 +1,39 @@
 import React, { useState } from 'react';
 import {
-  Collapse,
   Navbar,
-  NavbarToggler,
-  NavbarBrand,
   Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
-} from 'reactstrap';
-import { NavLink as RRNavLink } from 'react-router-dom';
-
+  Button
+} from 'react-bootstrap';
 import * as ROUTES from '../../constants/routes';
+import * as CONFIG from '../../constants/config';
 
 const TopNav = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const toggle = () => setIsOpen(!isOpen);
-  
-    return (
-      <div>
-        <Navbar color="primary" dark expand="md">
-          <NavbarBrand href="/">Educ'City</NavbarBrand>
-          <NavbarToggler onClick={toggle} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="mr-auto" navbar>
-              <NavItem>
-                <NavLink tag={RRNavLink} exact to={ROUTES.HOME}>Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/MaxenceGrenot/educ_city" target="_blank">GitHub</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>   
-        </Navbar>
-      </div>
-    );
-  }
-  
-  export default TopNav;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+        <Navbar.Brand href={ROUTES.HOME}>{CONFIG.WEBNAME}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href={ROUTES.CLASSES}>Classes</Nav.Link>
+            <Nav.Link href={ROUTES.GAMES}>Games</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href={ROUTES.SIGN_IN}>
+              <Button>Signin</Button>
+            </Nav.Link>
+            <Nav.Link eventKey={2} href={ROUTES.SIGN_UP}>
+              <Button>Signup</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+}
+
+export default TopNav;
