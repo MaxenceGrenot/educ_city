0ce1e09880b93c85b8e4f39df986ce45c01a7f27
Jérémie <jeremielegrand021@gmail.com>
2020-11-05 11:55:48 +0100

    [UPDATE]update package.json file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import TopNav from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import SignInPage from './components/SignIn/index';
+import SignUpPage from './components/SignUp/index';
 
 function App() {
   return (
@@ -14,6 +16,12 @@ function App() {
         <Switch>
           <Route exact path="/home">
             <Home />
+          </Route>
+          <Route exact path="/signin">
+            <SignInPage />
+          </Route>
+          <Route exact path="/signup">
+            <SignUpPage />
           </Route>
         </Switch>
       </>
