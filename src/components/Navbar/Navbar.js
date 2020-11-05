61bedf7289884860494757fa16b15a30724acabb
DuarteDosSantosNuno <67701344+DuarteDosSantosNuno@users.noreply.github.com>
2020-11-05 12:27:52 +0100

    [AUTH] SignIn and SignUp work

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,6 +6,9 @@ import {
 } from 'react-bootstrap';
 import * as ROUTES from '../../constants/routes';
 import * as CONFIG from '../../constants/config';
+import {AuthUserContext} from "../Session";
+import {Link} from "react-router-dom";
+import SignOutButton from "../SignOut";
 
 const TopNav = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -36,4 +39,49 @@ const TopNav = (props) => {
   );
 }
 
-export default TopNav;
+const Navigation = () => (
+    <div>
+      <AuthUserContext.Consumer>
+        {authUser =>
+            authUser ? <NavigationAuth /> : <NavigationNonAuth />
+        }
+      </AuthUserContext.Consumer>
+    </div>
+);
+
+const NavigationAuth = () => (
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+        <Navbar.Brand href={ROUTES.HOME}>{CONFIG.WEBNAME}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href={ROUTES.CLASSES}>Classes</Nav.Link>
+            <Nav.Link href={ROUTES.GAMES}>Games</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+);
+
+const NavigationNonAuth = () => (
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+        <Navbar.Brand href={ROUTES.HOME}>{CONFIG.WEBNAME}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav>
+            <Nav.Link href={ROUTES.SIGN_IN}>
+              <Button>Signin</Button>
+            </Nav.Link>
+            <Nav.Link eventKey={2} href={ROUTES.SIGN_UP}>
+              <Button>Signup</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+);
+
+//export default TopNav;
+export default Navigation;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+    </FirebaseContext.Provider>,
   document.getElementById('root')
 );
 
